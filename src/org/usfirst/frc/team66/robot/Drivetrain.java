0ae63c16ecf14bca1ecc92e7bf54f136aa844616
Dalton Smith <hitman26x@gmx.com>
2016-01-25 20:11:59 -0500

    Initial Drivetrain Code
    Assign one joystick per side
    Assign two motors per side
    Map joysticks to motor sides
    Add .class to gitignore

--- src/org/usfirst/frc/team66/robot/Drivetrain.java
+++ src/org/usfirst/frc/team66/robot/Drivetrain.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team66.robot;
 
-public class Drivetrain {
+import edu.wpi.first.wpilibj.Joystick;
 
+public class Drivetrain {
+	static Joystick leftJoystick = new Joystick(0);
+	static Joystick rightJoystick = new Joystick(1);
+	
+	static DrivetrainSide leftSide = new DrivetrainSide(true);
+	static DrivetrainSide rightSide = new DrivetrainSide(false);
+	
+	public static void updateDrivetrain() {
+		leftSide.set(leftJoystick.getY());
+		rightSide.set(rightJoystick.getY());
+	}
 }
--- src/org/usfirst/frc/team66/robot/DrivetrainSide.java
+++ src/org/usfirst/frc/team66/robot/DrivetrainSide.java
@@ -1,5 +1,30 @@
 package org.usfirst.frc.team66.robot;
 
-public class DrivetrainSide {
+import edu.wpi.first.wpilibj.Victor;
 
+public class DrivetrainSide {
+	private static final Victor LEFT_MOTOR1 = new Victor(0);
+	private static final Victor LEFT_MOTOR2 = new Victor(1);
+	private static final double LEFT_MOTOR1_SCALER = 1.0;
+	private static final double LEFT_MOTOR2_SCALER = 1.0;
+	private static final Victor RIGHT_MOTOR1 = new Victor(8);
+	private static final Victor RIGHT_MOTOR2 = new Victor(9);
+	private static final double RIGHT_MOTOR1_SCALER = 1.0;
+	private static final double RIGHT_MOTOR2_SCALER = 1.0;
+	
+	private boolean isLeftSide = false;
+	
+	public DrivetrainSide(boolean leftSide) {
+		isLeftSide = leftSide;
+	}
+	
+	public void set(double speed) {
+		if (isLeftSide) {
+			LEFT_MOTOR1.set(speed * LEFT_MOTOR1_SCALER);
+			LEFT_MOTOR2.set(speed * LEFT_MOTOR2_SCALER);
+		} else {
+			RIGHT_MOTOR1.set(speed * RIGHT_MOTOR1_SCALER);
+			RIGHT_MOTOR2.set(speed * RIGHT_MOTOR2_SCALER);
+		}
+	}
 }
