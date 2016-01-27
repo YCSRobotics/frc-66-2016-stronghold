83ba1c9327e689e4d1bb72335f1fb4c56b224f40
Dalton Smith <hitman26x@gmx.com>
2016-01-26 19:10:57 -0500

    Joystick Smoothing

--- src/org/usfirst/frc/team66/robot/Drivetrain.java
+++ src/org/usfirst/frc/team66/robot/Drivetrain.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team66.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer.StaticInterface;
 
 public class Drivetrain {
 	static Joystick leftJoystick = new Joystick(0);
@@ -9,8 +10,22 @@ public class Drivetrain {
 	static DrivetrainSide leftSide = new DrivetrainSide(true);
 	static DrivetrainSide rightSide = new DrivetrainSide(false);
 	
+	static double leftSideSpeed = 0;
+	static double rightSideSpeed = 0;
+	
 	public static void updateDrivetrain() {
-		leftSide.set(leftJoystick.getY());
-		rightSide.set(rightJoystick.getY());
+		leftSide.set(smoothValue(leftJoystick.getY(), leftSideSpeed, true));
+		rightSide.set(smoothValue(rightJoystick.getY(), rightSideSpeed, false));
+	}
+	
+	private static double smoothValue(double newJoy, double oldJoy, boolean isLeftSide) {
+		double smoothedSpeed = 0.0;
+		smoothedSpeed = (newJoy + oldJoy)/2;
+		if (isLeftSide) {
+			leftSideSpeed = smoothedSpeed;
+		} else {
+			rightSideSpeed = smoothedSpeed;
+		}
+		return smoothedSpeed;
 	}
 }
