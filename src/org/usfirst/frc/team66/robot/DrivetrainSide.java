4a34b506da7bd8a62baf902a1db7fca8cb81bdf8
magicslayer <cajankowiak@wccnet.edu>
2016-01-30 11:54:41 -0500

    Move to single pwm per side

--- src/org/usfirst/frc/team66/robot/DrivetrainSide.java
+++ src/org/usfirst/frc/team66/robot/DrivetrainSide.java
@@ -1,16 +1,16 @@
 package org.usfirst.frc.team66.robot;
 
-import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class DrivetrainSide {
-	private static final Victor LEFT_MOTOR1 = new Victor(0);
-	private static final Victor LEFT_MOTOR2 = new Victor(1);
+	private static final VictorSP LEFT_MOTOR1 = new VictorSP(0);
+	//private static final Victor LEFT_MOTOR2 = new Victor(1);
 	private static final double LEFT_MOTOR1_SCALER = 1.0;
-	private static final double LEFT_MOTOR2_SCALER = 1.0;
-	private static final Victor RIGHT_MOTOR1 = new Victor(8);
-	private static final Victor RIGHT_MOTOR2 = new Victor(9);
+	//private static final double LEFT_MOTOR2_SCALER = 1.0;
+	private static final VictorSP RIGHT_MOTOR1 = new VictorSP(8);
+	//private static final Victor RIGHT_MOTOR2 = new Victor(9);
 	private static final double RIGHT_MOTOR1_SCALER = 1.0;
-	private static final double RIGHT_MOTOR2_SCALER = 1.0;
+	//private static final double RIGHT_MOTOR2_SCALER = 1.0;
 	
 	private boolean isLeftSide = false;
 	
@@ -21,10 +21,10 @@ public class DrivetrainSide {
 	public void set(double speed) {
 		if (isLeftSide) {
 			LEFT_MOTOR1.set(speed * LEFT_MOTOR1_SCALER);
-			LEFT_MOTOR2.set(speed * LEFT_MOTOR2_SCALER);
+			//LEFT_MOTOR2.set(speed * LEFT_MOTOR2_SCALER);
 		} else {
 			RIGHT_MOTOR1.set(speed * RIGHT_MOTOR1_SCALER);
-			RIGHT_MOTOR2.set(speed * RIGHT_MOTOR2_SCALER);
+			//RIGHT_MOTOR2.set(speed * RIGHT_MOTOR2_SCALER);
 		}
 	}
 }
