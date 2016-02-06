b853d034b3407f19d43800d4bebc160c8e590046
fayfai64 <fayfai64@gmail.com>
2016-02-06 17:13:14 -0500

    Initial Intake Code

--- src/org/usfirst/frc/team66/robot/InTake.java
+++ src/org/usfirst/frc/team66/robot/InTake.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team66.robot;
 
 public class InTake {
+	private static final Victor LEFT_FEED_MOTOR1 = new Victor(4);
+	private static final double LEFT_FEED_MOTOR1_SCALER = -1.00;
 
+	private static final Victor RIGHT_FEED_MOTOR1 = new Victor(5);
+	private static final double RIGHT_FEED_MOTOR1_SCALER = 1.00;
+	
+	private static final Victor LIFT_MOTOR1 = new Victor(6);
+	private static final double LIFT_MOTOR1_SCALER = 1.00;
+	
+	
+	
 }
