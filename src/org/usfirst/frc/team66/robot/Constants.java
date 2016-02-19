86d08601222c8c1a6143d2f1b293de4244f2ba03
cajankowiak-wcc <cajankowiak@wccnet.edu>
2016-02-18 20:36:43 -0500

    add Shooter Controller

--- src/org/usfirst/frc/team66/robot/Constants.java
+++ src/org/usfirst/frc/team66/robot/Constants.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Victor;
 
 public class Constants {
 	// Drivetrain Motors
-	public static final Joystick DRIVE_CONTROLLER = new Joystick(0);
+	public static final Joystick DRIVE_CONTROLLER_1 = new Joystick(0);
 	
 	public static final Victor LEFT_MOTOR = new Victor(0);
 	public static final double LEFT_MOTOR_SCALER = -0.5;
